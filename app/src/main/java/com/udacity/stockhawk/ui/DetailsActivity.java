290cf81915eae5ec125de27f6199732c56f80e59
teo <ramones.teo@gmail.com>
2017-04-08 20:58:28 +0300

    Displays Chart

--- app/src/main/java/com/udacity/stockhawk/ui/DetailsActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailsActivity.java
@@ -5,8 +5,23 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.udacity.stockhawk.R;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -17,6 +32,11 @@ public class DetailsActivity extends AppCompatActivity {
 
     private static final String INTENT_SYMBOL = "symbol";
     private static final String INTENT_HISTORY = "history";
+
+    private static final String DELIMITER_NEWLINE = "\n";
+    private static final String DELIMITER_COMMA = ",";
+
+    @BindView(R.id.linechart) LineChart chart;
 
     String history, symbol;
 
@@ -32,7 +52,22 @@ public class DetailsActivity extends AppCompatActivity {
         }
 
         setUpToolbar();
+        setUpLineChart();
 
+        initChart(formatHistoryData(history));
+
+    }
+
+    private TreeMap<Float, Float> formatHistoryData(String history) {
+        String[] historyData = history.split(DELIMITER_NEWLINE);
+
+        TreeMap<Float, Float> treeMap = new TreeMap<>();
+        for (String historyString : historyData) {
+            String[] day = historyString.split(DELIMITER_COMMA);
+            treeMap.put(Float.valueOf(day[0]), Float.valueOf(day[1]));
+        }
+
+        return treeMap;
     }
 
     @Override
@@ -52,5 +87,52 @@ public class DetailsActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
     }
+
+    private void setUpLineChart() {
+        chart.setTouchEnabled(true);
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+    }
+
+    /**
+     * Code from MPAndroidChart Sample : https://goo.gl/6xSxUu
+     */
+    private void initChart(TreeMap<Float, Float> map) {
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+
+        private SimpleDateFormat dateFormatter = new SimpleDateFormat("yy-MM-dd");
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+                return dateFormatter.format(new Date((long) value));
+            }
+        });
+
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        chart.getAxisRight().setEnabled(false);
+        chart.setDescription(null);
+
+        setData(map);
+    }
+
+    private void setData(TreeMap<Float, Float> map) {
+        ArrayList<Entry> values = new ArrayList<>();
+
+        for (HashMap.Entry<Float, Float> entry : map.entrySet()) {
+            values.add(new Entry(entry.getKey(), entry.getValue()));
+        }
+
+        LineDataSet set1 = new LineDataSet(values, symbol);
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        // create data object
+        LineData data = new LineData(dataSets);
+
+        chart.setData(data);
+    }
+
 
 }
